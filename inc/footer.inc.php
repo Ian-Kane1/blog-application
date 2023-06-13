3e90c4c9524a6bee8c57e465c3583e6f2c1b0644
Ian Kane <i.kane2@students.clark.edu>
2023-06-12 20:15:04 -0700

    Create footer.inc.php

---
+++ inc/footer.inc.php
@@ -0,0 +1,10 @@
+<div class="row text-center">
+    <footer>
+        <!-- footer copyright -->
+        &copy; 2023 by Ian kane
+    </footer>
+</div>
+</div> <!-- Closing for .container -->
+</body>
+
+</html>
